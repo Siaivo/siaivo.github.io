6b007ecfb2a05e29d93f1285e585c388812c4a03
Felix <midi2001@mail.ru>
2026-03-19 21:17:30 +0200

    refactor(person): remove subscription functionality from person module
    Removes subscribe button and related subscription logic from the person profile component. This aligns with recent changes removing premium account checks from bookmark and favorite components.

--- src/interaction/person/module/about.js
+++ src/interaction/person/module/about.js
@@ -5,10 +5,6 @@ import Utils from '../../../utils/utils'
 import Modal from '../../modal'
 import Controller from '../../../core/controller'
 import Noty from '../../noty'
-import Storage from '../../../core/storage/storage'
-import Account from '../../../core/account/account'
-import Manifest from '../../../core/manifest'
-import Arrays from '../../../utils/arrays'
 
 export default {
     onCreate: function(){
@@ -20,13 +16,9 @@ export default {
 
         this.prefix = Template.prefix(this.html, 'person-start')
 
-        this.subscribed = Storage.get('person_subscribes_id','[]').find(a=>a == this.data.id)
-
         Utils.imgLoad(this.prefix.img, this.data.profile_path ? TMDB.img(this.data.profile_path, 'w500') : this.data.img || 'img/img_broken.svg', (img)=>{
             img.addClass('loaded')
         })
-
-        if(window.lampa_settings.account_use) this.emit('subscribe', this.subscribed)
 
         this.html.find('.button--info').on('hover:enter',()=>{
             if(this.data.biography){
@@ -47,50 +39,5 @@ export default {
         }).on('hover:focus hover:enter hover:hover',(e)=>{
             this.last = e.target
         })
-
-        if(window.lampa_settings.account_use){
-            this.html.find('.button--subscribe').on('hover:enter',()=>{
-                if(!Account.Permit.access) return Account.Modal.account()
-                
-                let subscribes = Storage.get('person_subscribes_id','[]')
-
-                this.subscribed = subscribes.find(a=>a == this.data.id)
-
-                if(!this.subscribed && !Account.hasPremium()) return Account.Modal.premium()
-
-                let person = Arrays.clone(this.data)
-
-                // Пихают там всякие иконки и прочее, удаляем лишнее
-                delete person.biography
-
-                Lampa.Network.silent(Utils.protocol() + Manifest.cub_domain + '/api/person/' + (this.subscribed ? 'unsubscribe' : 'subscribe'), ()=>{
-                    if(this.subscribed) Arrays.remove(subscribes, this.data.id)
-                    else if(subscribes.indexOf(this.data.id) == -1) subscribes.push(this.data.id)
-
-                    Storage.set('person_subscribes_id', subscribes)
-
-                    this.subscribed = subscribes.find(a=>a == this.data.id)
-
-                    this.emit('subscribe', this.subscribed)
-                },(err)=>{
-                    if(err.responseJSON && err.responseJSON.code == 555) Account.Modal.premium()
-                    else Noty.show(Lang.translate('subscribe_error'))
-                },{
-                    person: JSON.stringify(person)
-                },{
-                    headers: {
-                        token: Account.Permit.token
-                    }
-                })
-            }).on('hover:focus hover:enter hover:hover',(e)=>{
-                this.last = e.target
-            })
-        }
-        else this.html.find('.button--subscribe').remove()
-    },
-
-    onSubscribe: function(subscribed){
-        this.html.find('.button--subscribe svg path:nth-of-type(2)').setAttribute('fill', subscribed ? 'currentColor' : 'transparent')
-        this.html.find('.button--subscribe span').text(Lang.translate(subscribed ? 'title_unsubscribe' : 'title_subscribe'))
     }
 }
