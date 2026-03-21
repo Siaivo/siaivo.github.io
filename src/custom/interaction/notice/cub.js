9cbf10e513b1b388942e76621bf7cf625d45acc6
Felix <midi2001@mail.ru>
2026-03-21 07:40:54 +0200

    feat(notice): remove cub tab from notice display on init and open
    Modified Notice.init and Notice.open to automatically remove the 'cub' tab from notices by deleting the 'cub' class and changing display from 'cub' to 'all'. This ensures the cub tab is hidden when users initialize or open notices.

--- src/custom/interaction/notice/cub.js
+++ src/custom/interaction/notice/cub.js
@@ -1,4 +1,27 @@
+import Notice from '../../../interaction/notice/notice'
 import NoticeCub from '../../../interaction/notice/cub'
+
+function removeCubTab(notice) {
+    if (notice.classes && notice.classes.cub) {
+        delete notice.classes.cub
+    }
+
+    if (notice.display === 'cub') {
+        notice.display = 'all'
+    }
+}
+
+let init = Notice.init.bind(Notice)
+Notice.init = function() {
+    init()
+    removeCubTab(this)
+}
+
+let open = Notice.open.bind(Notice)
+Notice.open = function() {
+    removeCubTab(this)
+    open()
+}
 
 NoticeCub.prototype.update = function() {
     this.notices = []
