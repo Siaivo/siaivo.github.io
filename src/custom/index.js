72fdc589d36431f00bdd0110f30626a174007cef
Felix <midi2001@mail.ru>
2026-06-22 09:33:52 +0300

    refactor(custom): disable player compatibility layer import

--- src/custom/index.js
+++ src/custom/index.js
@@ -25,7 +25,7 @@ import './interaction/advert/vast_manager'
 import './lang/meta-apply'
 import './interaction/news-loader'
 import './interaction/profile-window'
-import './interaction/player-compatibility'
+//import './interaction/player-compatibility'
 import './app/torrent-badges'
 
 
