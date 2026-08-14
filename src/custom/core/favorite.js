3693cd67da7a9b3cbf1d8af4b4d03f013a5ee065
Oleksandr Zhyzhchenko <zhyzhchenko.ag@gmail.com>
2026-08-14 11:59:54 +0300

    Fix compatibility with the custom favs plugin

--- src/custom/core/favorite.js
+++ src/custom/core/favorite.js
@@ -67,167 +67,119 @@ function restorePersonCards(saved) {
     return restored
 }
 
-// 1. Override Favorite.all using property definition to prevent other plugins from breaking it
+// Обгортки нижче — звичайне присвоєння, а не accessor-властивість. Геттер, що
+// повертає обгортку, разом із сеттером, який перезаписує саме внутрішню ціль,
+// утворює цикл для будь-якого плагіна, що робить класичне
+// `var orig = Favorite.x; Favorite.x = function(){ orig.apply(...) }` —
+// його функція потрапляє в original_*, і виклик іде по колу до RangeError.
+
+// 1. Override Favorite.all
 let original_all = Favorite.all
-Object.defineProperty(Favorite, 'all', {
-    get: function() {
-        return function() {
-            let res = original_all.apply(this, arguments) || {}
-            if (!res.persons) res.persons = []
-            let local = getLocalPersons()
-            local.forEach(person => {
-                if (!res.persons.some(p => p.id == person.id)) {
-                    res.persons.push(person)
-                }
-            })
-            return res
+Favorite.all = function() {
+    let res = original_all.apply(this, arguments) || {}
+    if (!res.persons) res.persons = []
+    let local = getLocalPersons()
+    local.forEach(person => {
+        if (!res.persons.some(p => p.id == person.id)) {
+            res.persons.push(person)
         }
-    },
-    set: function(val) {
-        original_all = val
-    },
-    configurable: true,
-    enumerable: true
-})
+    })
+    return res
+}
 
-// 2. Override Favorite.check using property definition
+// 2. Override Favorite.check
 let original_check = Favorite.check
-Object.defineProperty(Favorite, 'check', {
-    get: function() {
-        return function(card) {
-            let res = original_check.apply(this, arguments) || {}
-            let data = Favorite.full()
-            let is_person = card.profile_path || card.known_for_department || typeof card.gender !== 'undefined' || (data.persons && data.persons.indexOf(card.id) > -1)
-            if(is_person){
-                res.persons = data.persons && data.persons.indexOf(card.id) > -1
-                if(res.persons) res.any = true
-            }
-            return res
-        }
-    },
-    set: function(val) {
-        original_check = val
-    },
-    configurable: true,
-    enumerable: true
-})
+Favorite.check = function(card) {
+    let res = original_check.apply(this, arguments) || {}
+    let data = Favorite.full()
+    let is_person = card.profile_path || card.known_for_department || typeof card.gender !== 'undefined' || (data.persons && data.persons.indexOf(card.id) > -1)
+    if(is_person){
+        res.persons = data.persons && data.persons.indexOf(card.id) > -1
+        if(res.persons) res.any = true
+    }
+    return res
+}
 
-// 3. Override Favorite.toggle using property definition
+// 3. Override Favorite.toggle
 let original_toggle = Favorite.toggle
-Object.defineProperty(Favorite, 'toggle', {
-    get: function() {
-        return function(where, card) {
-            if(where === 'persons'){
-                let data = Favorite.full()
-                if(!data.persons) data.persons = []
-                let idx = data.persons.indexOf(card.id)
-                if(idx > -1){
-                    Arrays.remove(data.persons, card.id)
-                    let status = Favorite.check(card)
-                    if(!status.any){
-                        for (let i = 0; i < data.card.length; i++) {
-                            if(data.card[i].id == card.id) {
-                                data.card.splice(i, 1)
-                                break
-                            }
-                        }
-                    }
-                } else {
-                    Arrays.insert(data.persons, 0, card.id)
-                    if(!data.card.find(c => c.id == card.id)){
-                        let clean = Arrays.clone(card)
-                        delete clean.biography
-                        Arrays.insert(data.card, 0, Utils.clearCard(clean))
+Favorite.toggle = function(where, card) {
+    if(where === 'persons'){
+        let data = Favorite.full()
+        if(!data.persons) data.persons = []
+        let idx = data.persons.indexOf(card.id)
+        if(idx > -1){
+            Arrays.remove(data.persons, card.id)
+            let status = Favorite.check(card)
+            if(!status.any){
+                for (let i = 0; i < data.card.length; i++) {
+                    if(data.card[i].id == card.id) {
+                        data.card.splice(i, 1)
+                        break
                     }
                 }
-                Lampa.Storage.set('favorite', data)
-                Lampa.Listener.send('state:changed', {
-                    target: 'favorite',
-                    reason: 'update',
-                    method: idx > -1 ? 'remove' : 'add',
-                    type: 'persons',
-                    card: card
-                })
-                return idx > -1 ? false : true
             }
-
-            let saved  = snapshotPersonCards()
-            let result = original_toggle.apply(this, arguments)
-
-            restorePersonCards(saved)
-
-            return result
+        } else {
+            Arrays.insert(data.persons, 0, card.id)
+            if(!data.card.find(c => c.id == card.id)){
+                let clean = Arrays.clone(card)
+                delete clean.biography
+                Arrays.insert(data.card, 0, Utils.clearCard(clean))
+            }
         }
-    },
-    set: function(val) {
-        original_toggle = val
-    },
-    configurable: true,
-    enumerable: true
-})
+        Lampa.Storage.set('favorite', data)
+        Lampa.Listener.send('state:changed', {
+            target: 'favorite',
+            reason: 'update',
+            method: idx > -1 ? 'remove' : 'add',
+            type: 'persons',
+            card: card
+        })
+        return idx > -1 ? false : true
+    }
+
+    let saved  = snapshotPersonCards()
+    let result = original_toggle.apply(this, arguments)
+
+    restorePersonCards(saved)
+
+    return result
+}
 
 // 4. Override Favorite.remove — захищаємо картки персон при прямому видаленні
 let original_remove = Favorite.remove
-Object.defineProperty(Favorite, 'remove', {
-    get: function() {
-        return function(where, card){
-            let saved  = snapshotPersonCards()
-            let result = original_remove.apply(this, arguments)
+Favorite.remove = function(where, card){
+    let saved  = snapshotPersonCards()
+    let result = original_remove.apply(this, arguments)
 
-            restorePersonCards(saved)
+    restorePersonCards(saved)
 
-            return result
-        }
-    },
-    set: function(val) {
-        original_remove = val
-    },
-    configurable: true,
-    enumerable: true
-})
+    return result
+}
 
 // 4.1. Override Favorite.clear — захищаємо картки персон при очищенні категорії
 let original_clear = Favorite.clear
-Object.defineProperty(Favorite, 'clear', {
-    get: function() {
-        return function(where, card){
-            let saved  = snapshotPersonCards()
-            let result = original_clear.apply(this, arguments)
+Favorite.clear = function(where, card){
+    let saved  = snapshotPersonCards()
+    let result = original_clear.apply(this, arguments)
 
-            restorePersonCards(saved)
+    restorePersonCards(saved)
 
-            return result
-        }
-    },
-    set: function(val) {
-        original_clear = val
-    },
-    configurable: true,
-    enumerable: true
-})
+    return result
+}
 
-// 5. Override Favorite.get using property definition
+// 5. Override Favorite.get
 let original_get = Favorite.get
-Object.defineProperty(Favorite, 'get', {
-    get: function() {
-        return function(params) {
-            if(params && params.type === 'persons'){
-                let res = original_get.apply(this, arguments) || []
-                if(!Array.isArray(res)) res = []
-                let local = getLocalPersons()
-                local.forEach(person => {
-                    if(!res.some(p => p.id == person.id)){
-                        res.push(person)
-                    }
-                })
-                return res
+Favorite.get = function(params) {
+    if(params && params.type === 'persons'){
+        let res = original_get.apply(this, arguments) || []
+        if(!Array.isArray(res)) res = []
+        let local = getLocalPersons()
+        local.forEach(person => {
+            if(!res.some(p => p.id == person.id)){
+                res.push(person)
             }
-            return original_get.apply(this, arguments)
-        }
-    },
-    set: function(val) {
-        original_get = val
-    },
-    configurable: true,
-    enumerable: true
-})
+        })
+        return res
+    }
+    return original_get.apply(this, arguments)
+}
